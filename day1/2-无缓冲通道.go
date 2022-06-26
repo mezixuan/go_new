7c555614afcc6df49cdaa717ae50c1d6c94ef0bf
赵津樊 <814763991@qq.com>
2022-06-26 12:18:45 +0800

    多go程，管道

---
+++ day1/2-无缓冲通道.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//创建管道，创建一个装数字的管道 ==》 channel
+	numChan := make(chan int) //使用管道时，一定要make，同map一样
+	//strChan := make(chan string) //装字符串的管道
+
+	go func() {
+		for i := 0; i < 50; i++ {
+			data := <-numChan
+			fmt.Println("子go程1,读出数据====>data：", data)
+		}
+	}()
+
+	go func() {
+		for i := 0; i < 20; i++ {
+			numChan <- i
+			fmt.Println("子go程2,写入数据====>data：", i)
+		}
+	}()
+
+	//创建两个go程，父亲写数据，儿子读数据
+	for i := 0; i < 30; i++ {
+		numChan <- i
+		fmt.Println("这是主go程，写入数据：", i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
--- day1/go_exit.go
+++ day1/go_exit.go
@@ -8,14 +8,19 @@ import (
 
 func main() {
 	go func() {
-		func() {
-			fmt.Println("这是子go程内部的函数")
-			//os.Exit(-1)
-			runtime.Goexit()
+		go func() {
+			func() {
+				fmt.Println("这是子go程内部的函数")
+				//os.Exit(-1)
+				runtime.Goexit()
+			}()
+			fmt.Println("子go程结束！")
+			fmt.Println("go 2222222")
 		}()
-		fmt.Println("子go程结束！")
+		time.Sleep(2 * time.Second)
+		fmt.Println("go 1111111")
 	}()
-	fmt.Println("这里是主go程")
-	time.Sleep(5)
-	fmt.Println("OVER")
+	fmt.Println("这是主go程")
+	time.Sleep(3 * time.Second)
+	fmt.Println("Over!")
 }
