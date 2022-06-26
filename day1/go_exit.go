40eb6ef74d7bdb9f124c70abe79855130fbfeb94
赵津樊 <814763991@qq.com>
2022-06-26 11:46:29 +0800

    second

--- day1/go_exit.go
+++ day1/go_exit.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
 func main() {
-	fmt.Println("nihao")
+	go func() {
+		func() {
+			fmt.Println("这是子go程内部的函数")
+			//os.Exit(-1)
+			runtime.Goexit()
+		}()
+		fmt.Println("子go程结束！")
+	}()
+	fmt.Println("这里是主go程")
+	time.Sleep(5)
+	fmt.Println("OVER")
 }
